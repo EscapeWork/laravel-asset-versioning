f7117665f22df9fc3672edd3dabcef3b987b964b
Luis Dalmolin <luis.nh@gmail.com>
2015-06-01 09:22:08 -0300

    Added a creation of dist directories recursive in case they dont exists)

--- src/SymLinker.php
+++ src/SymLinker.php
@@ -1,11 +1,19 @@
 <?php namespace EscapeWork\Assets;
 
+use File;
+
 class SymLinker
 {
 
     public function link($target, $link)
     {
-        symlink($origin_dir, $dist_dir);
+        $base_dir = explode('/', $link);
+        array_pop($base_dir);
+        File::makeDirectory(implode('/', $base_dir), 0755, true);
+
+        if (is_dir($target)) {
+            symlink($target, $link);
+        }
     }
 
     public function unlink($link)
